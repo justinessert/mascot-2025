e96f9112601ede02779ea4e70b6fc222a1ae5598
Justin Essert <justinkessert@gmail.com>
2025-03-02 18:49:29 -0500

    added google analytics

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter, Routes } from '@angular/router';
+import { provideRouter, Routes, Router, NavigationEnd } from '@angular/router';
 import { AppComponent } from './app/app.component';
 import { BracketComponent } from './app/bracket/bracket.component';
 import { HomeComponent } from './app/home/home.component';
@@ -13,5 +13,12 @@ const routes: Routes = [
 
 bootstrapApplication(AppComponent, {
   providers: [provideRouter(routes)],
-})
-  .catch(err => console.error(err));
+}).then(app => {
+  const router = app.injector.get(Router);
+  
+  router.events.subscribe(event => {
+    if (event instanceof NavigationEnd) {
+      (window as any).gtag('config', 'G-JWWPRW6P12', { page_path: event.urlAfterRedirects });
+    }
+  });
+}).catch(err => console.error(err));
