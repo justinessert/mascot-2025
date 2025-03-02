c4ed907531860a9094e261c11d742316358ae080
Justin Essert <justinkessert@gmail.com>
2025-03-02 17:48:10 -0500

    Fixed hover issues on mobile devices

--- src/app/winner-selection/winner-selection.component.ts
+++ src/app/winner-selection/winner-selection.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, ElementRef, Renderer2, AfterViewInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReplaceUnderscorePipe } from '../replace-underscore.pipe';
 
@@ -9,11 +9,37 @@ import { ReplaceUnderscorePipe } from '../replace-underscore.pipe';
   templateUrl: './winner-selection.component.html',
   styleUrls: ['./winner-selection.component.css']
 })
-export class WinnerSelectionComponent {
+export class WinnerSelectionComponent implements AfterViewInit, OnDestroy {
   @Input() currentMatchup: any[] = [];
   @Output() winnerSelected = new EventEmitter<any>();
 
+  private touchListener: any;
+
+  constructor(private renderer: Renderer2) {}
+
+  ngAfterViewInit() {
+    // ✅ Listen for first touch event to disable hover
+    this.touchListener = () => this.disableHoverOnTouch();
+    window.addEventListener('touchstart', this.touchListener, { passive: true });
+  }
+
+  ngOnDestroy() {
+    // Remove listener when component is destroyed
+    window.removeEventListener('touchstart', this.touchListener);
+  }
+
+  disableHoverOnTouch() {
+    this.renderer.addClass(document.body, 'disable-hover');
+
+    // Re-enable hover after a delay (when new images load)
+    setTimeout(() => {
+      this.renderer.removeClass(document.body, 'disable-hover');
+    }, 500);
+  }
+
   selectWinner(winner: any) {
+    // Prevent hover effects after selection
+    this.disableHoverOnTouch();
     this.winnerSelected.emit(winner);
   }
 }
