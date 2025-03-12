5232d70af665f7a729011d115a02bf48ee4e397a
Justin Essert <justinkessert@gmail.com>
2025-03-11 23:20:57 -0400

    got final four centered

--- src/app/full-bracket/full-bracket.component.ts
+++ src/app/full-bracket/full-bracket.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BracketSegmentComponent } from '../bracket-segment/bracket-segment.component';
 import { ReplaceUnderscorePipe } from '../replace-underscore.pipe';
+import { BracketService } from '../services/bracket.service';
 
 @Component({
   selector: 'app-full-bracket',
@@ -13,4 +14,15 @@ import { ReplaceUnderscorePipe } from '../replace-underscore.pipe';
 export class FullBracketComponent {
   leftRegions = ['east', 'west']; // Normal order
   rightRegions = ['midwest', 'south']; // Reverse order
+
+  constructor(private bracketService: BracketService) {}
+
+  getFinalFourMatchups(){
+    let bracket = this.bracketService.getRegionBracket("final_four");
+    let semiFinalLeft = [bracket[0][0], bracket[0][1]]
+    let semiFinalRight = [bracket[0][2], bracket[0][3]]
+    let final = [bracket[1][0], bracket[1][1]]
+    console.log("matchups: {}", [semiFinalLeft, final, semiFinalRight])
+    return [semiFinalLeft, final, semiFinalRight]
+  }
 }
