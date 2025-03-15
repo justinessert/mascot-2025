eb92601138372530e9045cf02e39f3498e31779e
Justin Essert <justinkessert@gmail.com>
2025-03-15 15:35:15 -0400

    fixed bracket viewing bug

--- src/app/bracket-segment/bracket-segment.component.ts
+++ src/app/bracket-segment/bracket-segment.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, Router } from '@angular/router';
 import { BracketService } from '../services/bracket.service';
@@ -11,11 +11,12 @@ import { MatchupComponent } from '../matchup/matchup.component';
   templateUrl: './bracket-segment.component.html',
   styleUrl: './bracket-segment.component.css'
 })
-export class BracketSegmentComponent implements OnInit {
+export class BracketSegmentComponent implements OnInit, OnChanges {
   @Input() currentRegion: string | null = null;
   @Input() reverseOrder: boolean = false;
   @Input() roundHeaders: boolean = true;
   bracket: any[][] = [];
+  roundNames: string[] = [];
 
   constructor(private route: ActivatedRoute, private router: Router, private bracketService: BracketService) {}
 
@@ -28,8 +29,23 @@ export class BracketSegmentComponent implements OnInit {
     });
   }
 
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['currentRegion'] && !changes['currentRegion'].firstChange) {
+      console.log("🔄 Region changed:", this.currentRegion);
+      this.initialize();
+    }
+  }
+
   initialize() {
-    this.bracket = this.bracketService.getRegionBracket(this.currentRegion || "east");
+    console.log("test")
+    let region_name = this.currentRegion || "east"
+    this.bracket = this.bracketService.getRegionBracket(region_name);
+    let n_rounds = this.bracket.length
+    if (region_name == "final_four") {
+      this.roundNames = ["Final Four", "Championship"];
+    } else {
+      this.roundNames = Array.from({ length: n_rounds }, (_, i) => `Round ${i + 1}`);
+    }
   }
 
   getMatchupPairs(round: any[]): any[][] {
