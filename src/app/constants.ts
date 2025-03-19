5ee308b85c12c95ad5124d9ea3f3fd230813a459
Justin Essert <justinkessert@gmail.com>
2025-03-19 17:59:47 -0400

    added nc game

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -64,7 +64,7 @@ export const regionOrder: Record<number, string[]> = {
 export const firstFourMapping: Record<number, Record<string, string>> = {
   2024: {},
   2025: {
-    "san_diego_state_or_north_carolina": "san_diego_state_or_north_carolina",
+    "san_diego_state_or_north_carolina": "north_carolina",
     "alabama_state_or_saint_francis_u": "alabama_state",
     "american_or_mount_saint_marys": "american_or_mount_saint_marys",
     "texas_or_xavier": "texas_or_xavier",
